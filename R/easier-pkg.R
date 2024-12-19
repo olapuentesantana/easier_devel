e5430a2cf5e68ecba943b53741c72b17fa543929
Federico Marini <marinif@uni-mainz.de>
2024-12-19 21:50:53 +0100

    alt doc way in the package main manpage

--- R/easier-pkg.R
+++ R/easier-pkg.R
@@ -11,13 +11,14 @@
 #' @importFrom graphics abline lines
 #' @importFrom utils View data
 #'
-#' @name easier-pkg
-#' @docType package
 #' @references Lapuente-Santana, Oscar, Maisa van Genderen,
 #' Peter A. J. Hilbers, Francesca Finotello, and Federica Eduati.
 #' 2021. “Interpretable Systems Biomarkers Predict Response to
 #' Immune-Checkpoint Inhibitors.” Patterns, 100293.
 #' https://doi.org/10.1016/j.patter.2021.100293.
-NULL
+#' 
+#' @name easier-pkg
+#' @keywords internal
+"_PACKAGE"
 
 globalVariables(c("feature", "istop", "threshold", "weight"))
